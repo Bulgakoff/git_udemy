badafcc5c0cf659659a60c7a6204f74494f99422
Bulgakoff <hlbu@yandex.ru>
2019-12-08 21:29:54 +0300

    polimorf

--- lessson07_polimorpthism/polimor.py
+++ lessson07_polimorpthism/polimor.py
@@ -1,24 +1,24 @@
 class Shape:
 
     def __init__(self):
-        print('Shape created')
+        print('Shape created in Shape')
 
     def draw(self):
-        print('Deawing the shape')
+        print('Deawing the shape  in Shape')
 
     def area(self):
-        print('calc area')
+        print('calc area  in Shape')
 
     def perimetr(self):
-        print('calc perimetr')
+        print('calc perimetr  in Shape')
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        Shape.__init__(self)# вызывается конструктор базового и передаем туда себя
+        Shape.__init__(self)  # вызывается конструктор базового и передаем туда себя
         self.width = width
         self.height = height
-        print('rectagle draw')
+        print('rectagle draw in Rectangle')
         # Shape.area(self)  # леголеголеголеголеголеголего для преопределения вызываю area передаю туда себя
         # Shape.perimetr(self) #вызов из конструкотора
         # Shape.draw(self) #вызов из конструкотора
@@ -30,12 +30,45 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
     def draw(self):
-        print(f'Drawing rectangle with width =  {self.width} and higth = {self.height}')
+        print(f'Drawing rectangle with width =  {self.width} and higth = {self.height} in Rectangle')
 
 
+import math
+
+
+class Triangle(Shape):
+    def __init__(self, a, b, c):
+        Shape.__init__(self)
+        self.a = a
+        self.b = b
+        self.c = c
+        print('Triangle craeted in Triangle')
+
+    def draw(self):
+        print(f'Drawing triangle in sides= {self.a}, {self.b}, {self.c}')
+
+    def area(self):
+        s = (self.a + self.b + self.c) / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def perimetr(self):
+        p = self.a + self.b + self.c
+        return p
+
+    @classmethod
+    def draw_next(cls, aa, bb, cc):
+        return cls(aa, bb, cc)
+
+
+#    ?//////////////////////////////////
+tri = Triangle(2, 6, 10)
+print(f'-------perimetr--------> {tri.perimetr()}')
+tri.draw()
+tri1 = Triangle.draw_next(11, 22, 33)
+tri1.draw()
 # /////////////////////////////////////
 sh = Shape()
 rect = Rectangle(12, 10)
-print(rect.draw())
-print(f'-------------area---===========>{rect.area()} sm')
-print(f'--------------perimetr--===========>{rect.perimetr()} sm')
+rect.draw()
+print(f'----------rect---area---===========>{rect.area()} sm')
+print(f'-----------rect---perimetr--===========>{rect.perimetr()} sm')
